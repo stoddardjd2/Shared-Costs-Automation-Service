600f82244ef12025e6faa7b29b60ebec9d85594c
Jared Stoddard <jared.stoddard2@gmail.com>
2025-09-25 09:59:05 -0700

    added variation for Test A

--- splitify-UI/src/components/global/landing-pages/figma/sections/Hero.jsx
+++ splitify-UI/src/components/global/landing-pages/figma/sections/Hero.jsx
@@ -60,7 +60,7 @@ lg:bg-[radial-gradient(62.5%_175.13%_at_97.01%_48.68%,_#075C7B_31.33%,_#022B3A_7
           </p>
 
           <CtaBtn
-          variation={"Landing-1.0-HERO"}
+          variation={"Landing-1.0-HERO-TEST-B"}
             className={`mt-[30px]
             mx-auto lg:mx-0
           text-white font-semibold px-6 py-3 
